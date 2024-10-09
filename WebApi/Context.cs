9bece62bece98b204ac3eb539ca35d6271f9cd42
smolapovs <smolapovs@gmail.com>
2024-10-09 09:31:04 +0300

    upd

--- WebApi/Context.cs
+++ WebApi/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 using WebApi.Models;
 
@@ -46,7 +47,72 @@ namespace WebApi
             };
 
             modelBuilder.Entity<TypeProduct>().HasData(typeProducts);
-        
+
+            //ЮЗЕРЫ
+            List<User> users = new List<User>()
+            {
+                new()
+                {
+                    Id= Guid.NewGuid(),
+                    IsClient = true,
+                    IsProvider = true,
+                    FIO = "Фамилия имя отчество",
+                    Discount = 20,
+                },
+                new()
+                {
+
+                }
+            };
+
+            modelBuilder.Entity<User>().HasData(users);
+
+            //ТОВАРЫ
+            List<Product> products = new List<Product>()
+            {
+                new()
+                {
+                    Id= Guid.NewGuid(),
+                    Count = 1,
+                    Price = 1,
+                    TypeId = typeProducts[0].Id,
+                }
+            };
+
+            modelBuilder.Entity<Product>().HasData(products);
+
+
+
+            //ЗАКАЗЫ
+            List<Order> orders = new List<Order>()
+            {
+                new()
+                {
+                    Id= Guid.NewGuid(),
+                    Count = 1,
+                    ProductId = products[0].Id,
+                    Sum = 1,
+                    UserId = users[0].Id
+                }
+            };
+
+            modelBuilder.Entity<Order>().HasData(orders);
+
+            List<News> news = new List<News>()
+            {
+                new()
+                {
+                    Id= Guid.NewGuid(),
+                    Title = "Title1",
+                    Created = DateTime.Now,
+                    Description = "Desc1",
+                }
+
+            };
+
+            modelBuilder.Entity<News>().HasData(news);
+
+
         }
 
         public DbSet<Product> Products { get; set; }
